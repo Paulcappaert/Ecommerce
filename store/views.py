2a7d211b5e441279eef2d3eaae60eb239ae69732
paulcappaert <paul.cappaert@gmail.com>
2019-02-22 16:13:44 -0500

    added product page

--- store/views.py
+++ store/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'store/index.html')
+	
+def productPage(request, pk):
+	return render(request, 'store/productPage.html')
 
 

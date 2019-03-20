f05cc0809a97453af83c606f899fb30ba98c883b
parsons12 <39281872+parsons12@users.noreply.github.com>
2019-03-20 16:28:33 -0400

    added add product page functionality

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
 	path('product/<int:pk>/', views.productPage, name='product'),
 	path('order/<int:pk>/', views.orderPage, name='order'),
+	path('add/', views.addProductPage, name='add'),
 	#path('confirmation', views.confirmationPage),
 	#path('Management', views.managementPage),
 	#path('orders', view.ordersPage),
